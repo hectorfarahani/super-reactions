c0f1bc5a1c50dabd1ddcfdfda42651a350ffc1fb
Hector <6560172+hector-codes@users.noreply.github.com>
2021-03-13 13:38:22 +0330

    Update version 1.3.1

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-var version = '1.3.0';
+var version = '1.3.1';
 var versioningFiles = [
   'super-reactions.php',
   'constants.php',
--- super-reactions.php
+++ super-reactions.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: Super Reactions
  * Description: Get meaningful reactions from users.
- * Version:     1.3.0
+ * Version:     1.3.1
  * Author:      Super Reactions Team
  * Text Domain: super-reactions
  * Domain Path: /languages
